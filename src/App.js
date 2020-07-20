5ad5eca48c082bc5797cda70da8bca6c9a03f3da
ekgru <katgruzdeva@gmail.com>
2020-07-20 13:24:11 +0300

    Исправлены недочеты, добавлена структура

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import MainPage from './MainPageContainer';
-import MainPageContainer from './MainPageContainer';
-import NavBar from './NavBar'
+import MainPageContainer from './MainPage/';
+import NavBar from './NavBar/'
 function App() {
   return (
     <div className="App">
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,14 +1,12 @@
 import React from "react";
-import geoIcon  from './resources/Geo.svg'
+import geoIcon  from '../resources/Geo.svg'
+import './Header.scss'
 export default class Header extends React.Component {
-  constructor() {
-    super();
-  }
   render() {
     return (
       <header className="header">
         <span className="header__site-name">Need for drive</span>
-        <span className="header__geolocation"><img className='header__geolocation__icon'src={geoIcon}/>{" "} Краснодар</span>
+        <span className="header__geolocation"><img className='header__geolocation__icon'src={geoIcon} alt='GeoLocation'/>{" "} Краснодар</span>
       </header>
     );
   }
