c04bf866a61a10ea6c545d94f416a43d0f4f54ae
ekgru <katgruzdeva@gmail.com>
2020-07-28 17:21:08 +0300

    Исправила баг верстки при цикличном прокручивании

--- src/components/Slider/Slide.js
+++ src/components/Slider/Slide.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Button from "../Button";
-export default function Slide({img, head, text} ) {
+export default function Slide({img, head, text, color} ) {
   const style = { backgroundImage: `url(${img})`};
   return (
     <div className="slide" style={style}>
       <span className="slide__content">
         <h1 className="slide__content__heading">{head}</h1>
         <p className="slide__content__text">{text}</p>
-        <Button title="Подробнее" type={"slide__content__btn "} />
+        <Button title="Подробнее" type={`slide__content__btn ${color}`} />
       </span>
     </div>
   );
