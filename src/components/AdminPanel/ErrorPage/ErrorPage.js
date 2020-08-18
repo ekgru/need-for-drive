51c20bf8e55165540f2926e83ec9265e1fd7bcf3
Ekaterina Gruzdeva <katgruzdeva@gmail.com>
2020-08-18 16:37:59 +0300

    Сделана верстка страницы ошибок

---
+++ src/components/AdminPanel/ErrorPage/ErrorPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ErrorPage.scss';
+export default function ErrorPage({ errorCode }) {
+  return (
+    <div className='error-page'>
+      <h1 className='error-page__code'>{errorCode || 'Неизвестная ошибка'}</h1>
+      <h2 className='error-page__description'>Что-то пошло не так</h2>
+      <h3 className='error-page__small-text'>
+        Попробуйте перезагрузить страницу
+      </h3>
+      <button className='admin__button blue'>Вернуться</button>
+    </div>
+  );
+}
