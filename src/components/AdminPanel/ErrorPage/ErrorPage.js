9913de592b1df940feafb29c60f308555b1645e3
Ekaterina Gruzdeva <katgruzdeva@gmail.com>
2020-08-25 13:05:54 +0300

    исправлено для мобилок

--- src/components/AdminPanel/ErrorPage/ErrorPage.js
+++ src/components/AdminPanel/ErrorPage/ErrorPage.js
@@ -3,7 +3,7 @@ import './ErrorPage.scss';
 export default function ErrorPage({ errorCode }) {
   return (
     <div className='error-page'>
-      <h1 className='error-page__code'>{errorCode || 'Неизвестная ошибка'}</h1>
+      <h1 className='error-page__code'>{errorCode || '404'}</h1>
       <h2 className='error-page__description'>Что-то пошло не так</h2>
       <h3 className='error-page__small-text'>
         Попробуйте перезагрузить страницу
