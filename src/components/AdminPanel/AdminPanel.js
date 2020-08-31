6f7729675675f5cba87a1624e1a709f424c77f9f
Ekaterina Gruzdeva <katgruzdeva@gmail.com>
2020-08-31 17:54:43 +0300

    Добавлена авторизация

--- src/components/AdminPanel/AdminPanel.js
+++ src/components/AdminPanel/AdminPanel.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Switch, Route, Redirect, useHistory } from 'react-router-dom';
 import Authorization from './Authorization';
 import './AdminPanel.scss';
 import Sidebar from './Sidebar';
@@ -9,54 +9,92 @@ import Orders from './Orders';
 import ErrorPage from './ErrorPage';
 import CarEditCard from './CarEditCard';
 import CityPointCard from './CityPointCard';
-export default class AdminPanel extends React.Component {
-  constructor() {
-    super();
-  }
-  render() {
-    return (
-      <div className='admin-panel'>
-        <Switch>
-          <Route path='/admin/autorization' component={Authorization} />
-          {new Date() < 0 ? <Redirect to='/admin/autorization' /> : ''}
-          <Route path='/admin/'>
-            <div className='admin-panel__container'>
-              <div className='admin-panel__container__topbar'>
-                <Topbar />
-              </div>
-              <div className='admin-panel__container__sidebar'>
-                <Sidebar />
-              </div>
-              <div className='admin-panel__container__content'>
-                <Switch>
-                  <Route exact path='/admin/'>
-                    <h1 className='admin__hello-text'>
-                      Добро пожаловать, {/* username || */ 'администратор'}
-                      !
-                    </h1>
-                  </Route>
-                  <Route
-                    exact
-                    path='/admin/car-edit-card'
-                    component={CarEditCard}
-                  />
-                  <Route exact path='/admin/orders' component={Orders} />
-                  <Route
-                    exact
-                    path='/admin/points'
-                    component={CityPointCard}
-                  />
+export default function AdminPanel() {
+  const [auth, setAuth] = useState(false);
+  const history = useHistory();
 
-                  <Route path='/admin/*' component={ErrorPage} />
-                </Switch>
-              </div>
-              <div className='admin-panel__container__bottombar'>
-                <Bottombar />
-              </div>
-            </div>
-          </Route>
-        </Switch>
-      </div>
-    );
+  useEffect(() => {
+    isAuth();
+  }, [history.location]);
+  function isAuth() {
+    function getCookie(name) {
+      const matches = document.cookie.match(
+        new RegExp(
+          '(?:^|; )' +
+            name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') +
+            '=([^;]*)',
+        ),
+      );
+      return matches ? matches[1] : undefined;
+    }
+    const api = 'http://api-factory.simbirsoft1.com/api/';
+    const headers = {
+      'X-Api-Factory-Application-Id': '5e25c641099b810b946c5d5b',
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + getCookie('basicToken'),
+    };
+    const data = { refresh_token: getCookie('refreshToken') };
+    fetch(`${api}auth/refresh`, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        document.cookie = `accessToken=${res.access_token}; max-age=${res.expires_in}; path='/need-fro-drive`;
+        document.cookie = `refreshToken=${res.refresh_token}; max-age=${res.expires_in}; path=/need-fro-drive`;
+        setAuth(true);
+      })
+      .catch((err) => {
+        console.error('ERROR', err);
+        setAuth(false);
+      });
   }
+
+  return (
+    <div className='admin-panel'>
+      <Switch>
+        <Route path='/admin/autorization'>
+          <Authorization />
+          {auth && <Redirect to='/admin/' />}
+        </Route>
+        <Route path='/admin/'>
+          <div className='admin-panel__container'>
+            <div className='admin-panel__container__topbar'>
+              <Topbar />
+            </div>
+            <div className='admin-panel__container__sidebar'>
+              <Sidebar />
+            </div>
+            <div className='admin-panel__container__content'>
+              <Switch>
+                {!auth && <Redirect to='/admin/autorization' />}
+                <Route exact path='/admin/'>
+                  <h1 className='admin__hello-text'>
+                    Добро пожаловать, {/* username || */ 'администратор'}!
+                  </h1>
+                </Route>
+                <Route
+                  exact
+                  path='/admin/car-edit-card'
+                  component={CarEditCard}
+                />
+                <Route exact path='/admin/orders' component={Orders} />
+                <Route
+                  exact
+                  path='/admin/points'
+                  component={CityPointCard}
+                />
+
+                <Route path='/admin/*' component={ErrorPage} />
+              </Switch>
+            </div>
+            <div className='admin-panel__container__bottombar'>
+              <Bottombar />
+            </div>
+          </div>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
--- src/components/AdminPanel/Authorization/Authorization.js
+++ src/components/AdminPanel/Authorization/Authorization.js
@@ -1,6 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
 import './Authorization.scss';
 export default function Authorization() {
+  const history = useHistory();
+  const [userPass, setPass] = useState('');
+  const [userLogin, setLogin] = useState('');
+  const [error, setError] = useState(false);
+  function handler(event) {
+    const { name, value } = event.target;
+    name === 'login' ? setLogin(value) : setPass(value);
+    setError(false);
+  }
+  function createRandomString(sumString) {
+    const symbolArr =
+      '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+    let randomString = '';
+    for (let i = 0; i < sumString; i++) {
+      const index = Math.floor(Math.random() * symbolArr.length);
+      randomString += symbolArr[index];
+    }
+    return randomString;
+  }
+  const basicToken = btoa(createRandomString(6) + ':4cbcea96de');
+  const api = 'http://api-factory.simbirsoft1.com/api/';
+  const headers = {
+    'X-Api-Factory-Application-Id': '5e25c641099b810b946c5d5b',
+    'Content-Type': 'application/json',
+    'Authorization': 'Basic ' + basicToken,
+  };
+
+  function auth(event) {
+    event.preventDefault();
+    const data = { username: userLogin, password: userPass };
+    fetch(`${api}auth/login`, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        console.log(res);
+        document.cookie = `basicToken=${basicToken};  max-age=${res.expires_in}`;
+        document.cookie = `accessToken=${res.access_token}; max-age=${res.expires_in}`;
+        document.cookie = `refreshToken=${res.refresh_token}; max-age=${res.expires_in}`;
+      })
+      .then(() => history.push('/admin/'))
+      .catch((err) => {
+        console.error('ERROR', err);
+        setError(true);
+      });
+  }
   return (
     <div className='authorization'>
       <span className='authorization__heading'>
@@ -9,26 +59,45 @@ export default function Authorization() {
       </span>
       <div className='authorization__login-block'>
         <h3 className='authorization__login-block__name'>Вход</h3>
-        <form className='authorization__login-block__form'>
-          <fieldset className='authorization__login-block__form__fieldset'>
-            <legend>Почта</legend>
+        <form onSubmit={auth} className='authorization__login-block__form'>
+          <fieldset
+            className={`authorization__login-block__form__fieldset  ${
+              error && 'error'
+            }`}
+          >
+            <legend>Логин</legend>
             <input
-              className='admin__input'
+              className={`admin__input`}
+              name='login'
               type='text'
+              value={userLogin}
+              onChange={handler}
             />
           </fieldset>
-          <fieldset className='authorization__login-block__form__fieldset'>
+          <fieldset
+            className={`authorization__login-block__form__fieldset  ${
+              error && 'error'
+            }`}
+          >
             <legend>Пароль</legend>
             <input
-              className='admin__input'
+              className={`admin__input`}
+              name='password'
+              value={userPass}
+              onChange={handler}
               type='password'
             />
+            {error && <span>Неверный логин или пароль</span>}
           </fieldset>
           <span className='authorization__login-block__form__buttons-block'>
             <a className='admin__link' href='#'>
               Запросить доступ
             </a>
-            <button className='admin__button blue' type='submit'>
+            <button
+              onClick={auth}
+              className='admin__button blue'
+              type='submit'
+            >
               Войти
             </button>
           </span>
